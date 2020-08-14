cb382b259eb942d6cca78b2e7a972265e8212c60
einobaka <61556425+einobaka@users.noreply.github.com>
2020-08-13 21:21:53 -0700

    started testing with postman first api route

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../db/db.json')
+
+// Router test
+router.get('/', (req, res) => { // gets entire JSON file 
+    // res.send(console.log(data))
+    res.json(data)
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require("path");
 const app = express();
 
 // Sets up the Express app to handle data
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Compiled App routes
-app.use('/api/db', require('./routes/api/db'));
+app.use('/api/db', require('./routes/api'));
 
 // Port handling
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhose:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
