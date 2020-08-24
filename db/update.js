0d28ecd604ede8b6b61d3ab3fae32a47351ae685
einobaka <61556425+einobaka@users.noreply.github.com>
2020-08-24 15:15:27 -0700

    added promisify to handle callbacks

--- db/update.js
+++ db/update.js
@@ -1,15 +1,40 @@
 const fs = require('fs');
-const data = require('../db/db');
+// const data = require('../db/db');
+const util = require("util");
+const uuid = require('uuid');
+
+// Promisify for response as promise object on callbacks
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 class Update {
 
-    create(note) {
-        
+    read() {
+        return readFile("db/db.json", "utf8")
     }
+
+    write(entry) {
+        return writeFile("test.txt", entry)
+    }
+
+    view() {
+        return this.read()
+    }
+
+
+    // view() {
+    //     let notes = [];
+    //     return notes = fs.readFile('db/db.json', 'utf8')
+    // }
+
+    // create(note) {
+    //     return fs.writeFile('db/db.json', JSON.stringify(note))
+    // }
+
 
     delete(id) {
         let current = data.filter(entry => entry.id !== id)
-        return fs.writeFileSync('db/db.json', JSON.stringify(current))
+        return fs.writeFile('db/db.json', JSON.stringify(current))
     }
 
 }
--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const data = require('../db/db');
 const uuid = require('uuid');
 const update = require('../db/update');
+const fs = require('fs');
 
 // Get request for api/notes
 router.get('/api/notes', (req, res) => {
 
-    update.view(data)
+    // Call for view on current notes
+    update.view()
+        .then((notes) => res.json(notes));
+
 
 });
 
@@ -21,8 +25,12 @@ router.post('/api/notes', (req, res) => {
         title: req.body.title,
         text: req.body.text
     }
-
     data.push(newEntry)
+
+    // update.create(newEntry)
+
+    // update.write(newEntry)
+
     res.json(data);
 
 });
@@ -31,11 +39,22 @@ router.post('/api/notes', (req, res) => {
 router.delete('/api/notes/:id', (req, res) => {
 
     const found = data.some(entry => entry.id === req.params.id)
+    console.log(found)
+
+    let filtered = data.filter(entry => entry.id !== req.params.id)
+
+    fs.writeFile('db/db.json', filtered)
+
+    // console.log(data)
+    // res.json(data)
 
     if (found) {
 
-        update.delete(req.params.id)
-        res.json(update.data)
+        // update.delete(data) 
+
+        // found.filter(entry => (entry.id !== req.params.id), entry)
+
+        // console.log(entry);
 
         // res.json({ msg: 'Entry Deleted', data: data.filter(entry => entry.id !== req.params.id) }) // filter through an array data
 
